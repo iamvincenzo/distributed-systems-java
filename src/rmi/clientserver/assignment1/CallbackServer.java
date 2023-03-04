55ea47642ff4b8feefdfe4f70ec1dfae975e8ca2
vince <43069944+iamvincenzo@users.noreply.github.com>
2023-03-04 11:48:21 +0100

    Update CallbackServer.java

--- src/rmi/clientserver/assignment1/CallbackServer.java
+++ src/rmi/clientserver/assignment1/CallbackServer.java
@@ -1,7 +1,6 @@
 package rmi.clientserver.assignment1;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
 import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
